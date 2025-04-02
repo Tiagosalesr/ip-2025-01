febe156ad5f5622002b29ba3d6ea897f86eae530
Tiagosalesr <tiago2007sales@gmail.com>
2025-04-02 20:09:54 -0300

    Update Questão03.go

--- Lista01go/Questão03.go
+++ Lista01go/Questão03.go
@@ -9,10 +9,10 @@ func main() {
 
 	fmt.Scanln(&n1, &n2, &n3)
 
-   if (n1 <= 9 && n1 >= 0 && n2 <= 9 && n3 >= 0 && n3 <= 9 && n3 >= 0 ) {
+   if (n1 <= 9 && n1 >= 0 && n2 <= 9 && n2 >= 0 && n3 <= 9 && n3 >= 0 ) {
 	numeroFinal := (n1 * 100) + (n2 * 10) 
-	fmt.Println(&numeroFinal,&n3,", ")
-	n4 := (n1 *100) + (n2 * 10) + n3
+	fmt.Println(numeroFinal,n3,", ")
+	n4 := numeroFinal + n3
 	fmt.Println(n4 * n4)
    } else {
    fmt.Println("DIGITO INVALIDO")
