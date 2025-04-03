2ef4a39e80ac9876c3cdb678d2ad5755bb7e78e3
Tiagosalesr <tiago2007sales@gmail.com>
2025-04-02 21:00:42 -0300

    Update Questão01.go

--- Lista01go/Questão01.go
+++ Lista01go/Questão01.go
@@ -1,8 +1,10 @@
 package main
-import fmt
+import "fmt"
 func main() {
 	var nota1, nota2, nota3 float64
-	fmt.Scanf("%f %f %f", &nota1, &nota2, &nota3)
+	fmt.Scanln(&nota1)
+	fmt.Scanln(&nota2)
+	fmt.Scanln(&nota3)
 	media := (nota1 + nota2 + nota3) / 3
 	fmt.Printf("MEDIA = %.2f\n", media)
 	if media >= 6 {
